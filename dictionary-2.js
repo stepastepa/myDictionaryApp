e9db76f5c6717e6c98de8e1e3f2bcd6c2ce58665
stepastepa <stepanisworking@gmail.com>
2025-08-04 18:17:21 +0300

    add additional dictionary with pronouns

---
+++ dictionary-2.js
@@ -0,0 +1,14 @@
+const additionalDictionary = [
+    { rus: 'я', heb: 'אני', speak: 'анИ', type: 'местоимение' },
+    { rus: 'ты (м)', heb: 'אתה', speak: 'атА', type: 'местоимение' },
+    { rus: 'ты (ж)', heb: 'את', speak: 'ат', type: 'местоимение' },
+    { rus: 'вы (м)', heb: 'אתם', speak: 'атЭм', type: 'местоимение' },
+    { rus: 'вы (ж)', heb: 'אתן', speak: 'атЭн', type: 'местоимение' },
+    { rus: 'мы', heb: 'אנחנו', speak: 'анАхну', type: 'местоимение' },
+    { rus: 'он', heb: 'הוא', speak: 'hу', type: 'местоимение' },
+    { rus: 'она', heb: 'היא', speak: 'hи', type: 'местоимение' },
+    { rus: 'они (м)', heb: 'הם', speak: 'hем', type: 'местоимение' },
+    { rus: 'они (ж)', heb: 'הן', speak: 'hен', type: 'местоимение' },
+];
+
+export { additionalDictionary };
