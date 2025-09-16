90d5939a2fea84dd0bd585ea01e4867cfc23e6b0
stepastepa <stepanisworking@gmail.com>
2025-09-16 18:46:43 +0300

    addd new dictionary file

---
+++ dictionary-3.js
@@ -0,0 +1,34 @@
+const curatedDictionary = [
+
+    ///////// 1-25 /////////////////////////
+    { rus: '', heb: '', speak: '', type: '' },
+    { rus: '', heb: '', speak: '', type: '' },
+    { rus: '', heb: '', speak: '', type: '' },
+    { rus: '', heb: '', speak: '', type: '' },
+    { rus: '', heb: '', speak: '', type: '' },
+    { rus: '', heb: '', speak: '', type: '' },
+    { rus: '', heb: '', speak: '', type: '' },
+    { rus: '', heb: '', speak: '', type: '' },
+    { rus: '', heb: '', speak: '', type: '' },
+    { rus: '', heb: '', speak: '', type: '' },
+    { rus: '', heb: '', speak: '', type: '' },
+    { rus: '', heb: '', speak: '', type: '' },
+    { rus: '', heb: '', speak: '', type: '' },
+    { rus: '', heb: '', speak: '', type: '' },
+    { rus: '', heb: '', speak: '', type: '' },
+    { rus: '', heb: '', speak: '', type: '' },
+    { rus: '', heb: '', speak: '', type: '' },
+    { rus: '', heb: '', speak: '', type: '' },
+    { rus: '', heb: '', speak: '', type: '' },
+    { rus: '', heb: '', speak: '', type: '' },
+    { rus: '', heb: '', speak: '', type: '' },
+    { rus: '', heb: '', speak: '', type: '' },
+    { rus: '', heb: '', speak: '', type: '' },
+    { rus: '', heb: '', speak: '', type: '' },
+    { rus: '', heb: '', speak: '', type: '' },
+
+    ///////// 25-50 /////////////////////////
+    
+];
+
+export { curatedDictionary };
